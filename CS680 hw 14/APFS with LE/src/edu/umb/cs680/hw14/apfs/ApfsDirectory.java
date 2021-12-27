df7cceb98bc456d3560e7fd239bd93f86cafdde6
shivangraikar <shivangraikar@gmail.com>
2021-12-26 22:03:36 -0500

    Final submission

---
+++ CS680 hw 14/APFS with LE/src/edu/umb/cs680/hw14/apfs/ApfsDirectory.java
@@ -0,0 +1,158 @@
+package edu.umb.cs680.hw14.apfs;
+
+import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedList;
+
+import edu.umb.cs680.hw14.fs.FSElement;
+public class ApfsDirectory extends ApfsElement{
+
+	private LinkedList<ApfsDirectory> directory = new LinkedList<>();
+	private LinkedList<ApfsElement> children = new LinkedList<>();
+	private LinkedList<ApfsFile> file = new LinkedList<>();
+	
+	public ApfsDirectory(ApfsDirectory parent, String name, int size, LocalDateTime creationTime, String OwnerName, LocalDateTime LD) {
+		super(parent, name, size, creationTime, OwnerName, LD);
+		if(parent!= null) {
+			parent.appendChild(this);
+		}
+	}
+	
+	public int CountChildren() {
+		int totalCount = 0;
+		for(@SuppressWarnings("unused") ApfsElement f: getChildren()) {
+			totalCount += 1;
+		}
+		return totalCount;
+	}
+	
+	public void appendChild(ApfsElement child) {
+		this.children.add(child);
+	}
+	
+	public LinkedList<ApfsElement> getChildren() {
+        Collections.sort(this.children, (ApfsElement arg1, ApfsElement arg2) -> arg1.getName().compareTo(arg2.getName()));
+		return this.children;
+	}
+	
+	public LinkedList<ApfsDirectory> getSubDirectory(){
+		for(FSElement s: getChildren()) {
+			if(s instanceof ApfsDirectory) {
+				directory.add((ApfsDirectory) s);
+			}
+		}
+		Collections.sort(directory, (ApfsElement arg1, ApfsElement arg2) -> arg1.getName().compareTo(arg2.getName()));
+		return directory;
+	}
+	
+	public LinkedList<ApfsFile> getFiles(){
+		for(FSElement s: getChildren()) {
+			if(s instanceof ApfsFile) {
+				file.add((ApfsFile) s);
+			}
+		}
+		Collections.sort(file, (ApfsElement arg1, ApfsElement arg2) -> arg1.getName().compareTo(arg2.getName()));
+		return file;
+	}
+	
+	public LinkedList<ApfsLink> getLinks() {
+		LinkedList<ApfsLink> listoflink = new LinkedList<ApfsLink>();
+		for(FSElement s : getChildren()) {
+			if(s instanceof ApfsLink)
+				listoflink.add((ApfsLink) s);
+		}
+		return listoflink;
+	}
+	
+	public int getTotalSize() {
+		int totalSize = 0;
+		for(ApfsElement f: getChildren()) {
+			if(f instanceof ApfsFile) {
+				totalSize += f.getSize();
+			}
+			if(f instanceof ApfsDirectory) {
+				totalSize += ((ApfsDirectory)f).getTotalSize();
+			}
+		}
+		return totalSize;
+	}
+	
+	public ApfsDirectory findDirectoryByName(String name) {
+		ApfsDirectory directory = null;
+		if(name.equals(getName())) {
+			directory = this;
+		}
+		else {
+			for(ApfsDirectory d: getSubDirectory()) {
+				if(directory == null) {
+					directory = d.findDirectoryByName(name);
+					if(name.equals(d.getName())) {
+						directory = d;
+						break;
+					}
+				}			
+			}
+		}
+		return directory;
+	}
+	
+	public ApfsFile findFileByName(String name) {
+		ApfsFile file = null;
+		for(ApfsFile f: getFiles()) {
+			if(name.equals(f.getName())) {
+				file = f;
+			}
+		}
+		if(file == null) {
+			for(ApfsDirectory d: getSubDirectory()) {
+				file = d.findFileByName(name);
+				if(file != null) {
+					break;
+				}
+			}
+		}
+		
+		return file;
+	}
+	
+	public ApfsLink findLinkByName(String name) {
+		ApfsLink link = null;
+		for(ApfsLink l : getLinks()) {
+			if(name.equals(l.getName()))
+				link = l;
+		}
+		if(link == null) {
+			for(ApfsDirectory dir : getSubDirectory()) {
+				link = dir.findLinkByName(name);
+				if(link != null)
+					break;
+			}
+		}
+		return link;
+	}
+	
+	public LinkedList<ApfsElement> getChildren(Comparator<ApfsElement> compare) {
+		LinkedList<ApfsElement> f = getChildren();
+		Collections.sort(f, compare);
+		return f;
+	}
+	
+	public LinkedList<ApfsDirectory> getSubDirectories(Comparator<ApfsElement> compare){
+		LinkedList<ApfsDirectory> dir = getSubDirectory();
+		Collections.sort(dir, compare);
+		return dir;
+	}
+	
+	public LinkedList<ApfsFile> getFiles(Comparator<ApfsElement> compare){
+		LinkedList<ApfsFile> f = getFiles();
+		Collections.sort(f, compare);
+		return f;
+	}
+	
+	@Override
+	public boolean isDirectory() {
+		return true;
+	}
+
+}
---
+++ CS680 hw 14/APFS with LE/test/src/edu/umb/cs680/hw14/apfs/ReverseAlphabeticalComparatorTest.java
@@ -0,0 +1,104 @@
+package edu.umb.cs680.hw14.apfs;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDateTime;
+import java.util.LinkedList;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class ReverseAlphabeticalComparatorTest {
+
+static LocalDateTime localTime = LocalDateTime.now();
+	
+	private String[] ApfsEleToString(LinkedList<ApfsElement> l) {
+		String[] info = new String[l.size()];
+		int i=0;
+		for(ApfsElement e: l) {
+			info[i] = e.getName();
+			//System.out.println(info[i]);
+			i++;
+		}
+        return info;
+		
+	}
+	
+	private String[] APFSFileToString(LinkedList<ApfsFile> l) {
+		String[] info = new String[l.size()];
+		int i=0;
+		for(ApfsFile e: l) {
+			info[i] = e.getName();
+			//System.out.println(info[i]);
+			i++;
+		}
+        return info;
+    }
+	
+	
+	@SuppressWarnings("unused")
+	@BeforeAll
+	public static void setUpFileStructure() {
+		APFS apfsFileSystem = APFS.getAPFSFileSystem();
+		ApfsDirectory root = (ApfsDirectory)FilesystemApfs.initFileSystem("root", 3500);
+		ApfsDirectory applications = new ApfsDirectory(root, "applications", 0, localTime, "Shivang", localTime);
+		ApfsDirectory home = new ApfsDirectory(root, "home", 0, localTime, "Shivang", localTime);
+		ApfsDirectory code = new ApfsDirectory(home, "code", 0, localTime, "Shivang", localTime);
+		ApfsDirectory pics = new ApfsDirectory(home, "pics", 0, localTime, "Shivang", localTime);
+		ApfsFile a = new ApfsFile(applications, "a", 350, localTime, "Shivang", localTime);
+		ApfsFile b = new ApfsFile(home, "b", 700, localTime, "Shivang", localTime);
+		ApfsFile c = new ApfsFile(code, "c", 800, localTime, "Shivang", localTime);
+		ApfsFile d = new ApfsFile(code, "d", 80, localTime, "Shivang", localTime);
+		ApfsFile e = new ApfsFile(pics, "e", 700, localTime, "Shivang", localTime);
+		ApfsFile f = new ApfsFile(pics, "f", 870, localTime, "Shivang", localTime);
+		ApfsFile g = new ApfsFile(pics, "g", 1, localTime, "Shivang", localTime);
+		ApfsLink x = new ApfsLink(home, "x", 0, localTime, "Shivang", localTime, applications);
+		ApfsLink y = new ApfsLink(code, "y", 0, localTime, "Shivang", localTime, a);
+	}
+	
+    @Test
+	public void ChildrenHomeDiretoryComparatorTest() {
+    	String[] expected = {"x", "pics","g", "code", "b"};
+		APFS FilesystemApfs = APFS.getAPFSFileSystem();
+		ApfsDirectory root = (ApfsDirectory)FilesystemApfs.getRootDir();
+		LinkedList<ApfsElement> l= root.findDirectoryByName("home").getChildren((ApfsElement arg1, ApfsElement arg2)-> -arg1.getName().compareTo(arg2.getName()));
+		assertArrayEquals(expected, ApfsEleToString(l));
+	}
+	
+	@Test
+	public void FilesInCodeDirectoryComparatorTest() {
+		String[] expected = {"d","c"};
+		APFS FilesystemApfs = APFS.getAPFSFileSystem();
+		ApfsDirectory root = (ApfsDirectory)FilesystemApfs.getRootDir();
+		LinkedList<ApfsFile> l= root.findDirectoryByName("code").getFiles((ApfsElement arg1, ApfsElement arg2)-> -arg1.getName().compareTo(arg2.getName()));
+		assertArrayEquals(expected, APFSFileToString(l));
+	}
+	
+	@Test
+	public void FileInPicsDirectoryComparatorTest() {
+		String[] expected = {"f","e"};
+		APFS FilesystemApfs = APFS.getAPFSFileSystem();
+		ApfsDirectory root = (ApfsDirectory)FilesystemApfs.getRootDir();
+		LinkedList<ApfsFile> l= root.findDirectoryByName("pics").getFiles((ApfsElement arg1, ApfsElement arg2)-> -arg1.getName().compareTo(arg2.getName()));
+		assertArrayEquals(expected, APFSFileToString(l));
+	}
+	
+	@Test
+	public void ChildrenInRootDirectoryComparatorTest() {
+		String[] expected = {"home","applications"};
+		APFS FilesystemApfs = APFS.getAPFSFileSystem();
+		ApfsDirectory root = (ApfsDirectory)FilesystemApfs.getRootDir();
+		LinkedList<ApfsElement> l= root.findDirectoryByName("root").getChildren((ApfsElement arg1, ApfsElement arg2)-> -arg1.getName().compareTo(arg2.getName()));
+		assertArrayEquals(expected, ApfsEleToString(l));
+	}
+	
+	@Test
+	public void FileInHomeDirectoryComparatorTest() {
+		String[] expected = {"g","b"};
+		APFS FilesystemApfs = APFS.getAPFSFileSystem();
+		ApfsDirectory root = (ApfsDirectory)FilesystemApfs.getRootDir();
+		LinkedList<ApfsFile> l= root.findDirectoryByName("home").getFiles((ApfsElement arg1, ApfsElement arg2)-> -arg1.getName().compareTo(arg2.getName()));
+		assertArrayEquals(expected, APFSFileToString(l));
+	}
+
+}
